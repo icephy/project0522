d737d634866e457dc0322197b30b0bbb6510ac6a
murphy <yppice.adc@gmail.com>
2024-06-08 20:52:16 +0800

    居酒屋 js 聲音檔

---
+++ js/izakaya.js
@@ -0,0 +1,127 @@
+// 播放聲音的函式
+function PlaySound(soundName) {
+  // 根據聲音名稱播放對應的聲音
+  // 任務卡A-001-會話練習 
+  switch(soundName) {
+    case 'hotelLessonOneAConv_001':
+      var hotelLessonOneAConv_001 = new Audio('https://icephy.github.io/project0522/audio/sound/hotel/A/conversation/hotelLessonOneAConv_001.mp3');
+      hotelLessonOneAConv_001.play();
+      break;
+
+  // 任務卡A-001-單字 
+    case 'hotelLessonOneAVocab_001':
+      var hotelLessonOneAVocab_001 = new Audio('https://icephy.github.io/project0522/audio/sound/hotel/A/vocabulary/hotelLessonOneAVocab_001.mp3');
+      hotelLessonOneAVocab_001.play();
+      break;
+
+  // 任務卡B-001-會話
+    case 'hotelLessonOneBConv_001':
+        var hotelLessonOneBConv_001 = new Audio('https://icephy.github.io/project0522/audio/sound/hotel/B/conversation/hotelLessonOneBConv_001.mp3');
+        hotelLessonOneBConv_001.play();
+      break;
+
+    // 任務卡C-001-單字
+    case 'hotelLessonOneCVocab_001':
+        var hotelLessonOneCVocab_001 = new Audio('https://icephy.github.io/project0522/audio/sound/hotel/C/vocabulary/hotelLessonOneCVocab_001.mp3');
+        hotelLessonOneCVocab_001.play();
+      break;
+
+    // 任務卡D-001-單字
+    case 'hotelLessonOneDVocab_001':
+        var hotelLessonOneDVocab_001 = new Audio('https://icephy.github.io/project0522/audio/sound/hotel/D/vocabulary/hotelLessonOneDVocab_001.mp3');
+        hotelLessonOneDVocab_001.play();
+      break;
+
+    // 短文聽力
+    case 'hotelShortTalksCard_001':
+        var hotelShortTalksCard_001 = new Audio('https://icephy.github.io/project0522/audio/sound/hotel/short_talks/hotelShortTalksCard_001.mp3');
+        hotelShortTalksCard_001.play();
+      break;
+
+    // 單字區（一）
+    case 'izakayaVocabSectionTabs_oshibori':
+        var izakayaVocabSectionTabs_oshibori = new Audio('https://icephy.github.io/project0522/audio/sound/izakaya/vocab_section/izakayaVocabSectionTabs_oshibori.mp3');
+        izakayaVocabSectionTabs_oshibori.play();
+      break;
+    case 'izakayaVocabSectionTabs_kozara':
+        var izakayaVocabSectionTabs_kozara = new Audio('https://icephy.github.io/project0522/audio/sound/izakaya/vocab_section/izakayaVocabSectionTabs_kozara.mp3');
+        izakayaVocabSectionTabs_kozara.play();
+      break;
+    case 'izakayaVocabSectionTabs_okawari ':
+        var izakayaVocabSectionTabs_okawari  = new Audio('https://icephy.github.io/project0522/audio/sound/izakaya/vocab_section/izakayaVocabSectionTabs_okawari .mp3');
+        izakayaVocabSectionTabs_okawari .play();
+      break;
+    case 'izakayaVocabSectionTabs_tsuikachuumon':
+        var izakayaVocabSectionTabs_tsuikachuumon = new Audio('https://icephy.github.io/project0522/audio/sound/izakaya/vocab_section/izakayaVocabSectionTabs_tsuikachuumon.mp3');
+        izakayaVocabSectionTabs_tsuikachuumon.play();
+      break;
+    case 'izakayaVocabSectionTabs_ryoushuusho':
+        var izakayaVocabSectionTabs_ryoushuusho = new Audio('https://icephy.github.io/project0522/audio/sound/izakaya/vocab_section/izakayaVocabSectionTabs_ryoushuusho.mp3');
+        izakayaVocabSectionTabs_ryoushuusho.play();
+      break;
+    case 'izakayaVocabSectionTabs_shinagire':
+        var izakayaVocabSectionTabs_shinagire = new Audio('https://icephy.github.io/project0522/audio/sound/izakaya/vocab_section/izakayaVocabSectionTabs_shinagire.mp3');
+        izakayaVocabSectionTabs_shinagire.play();
+      break;
+    case 'izakayaVocabSectionTabs_manseki':
+        var izakayaVocabSectionTabs_manseki = new Audio('https://icephy.github.io/project0522/audio/sound/izakaya/vocab_section/izakayaVocabSectionTabs_manseki.mp3');
+        izakayaVocabSectionTabs_manseki.play();
+      break;
+    case 'izakayaVocabSectionTabs_shokki':
+        var izakayaVocabSectionTabs_shokki = new Audio('https://icephy.github.io/project0522/audio/sound/izakaya/vocab_section/izakayaVocabSectionTabs_shokki.mp3');
+        izakayaVocabSectionTabs_shokki.play();
+      break;
+    case 'izakayaVocabSectionTabs_fuooku':
+        var izakayaVocabSectionTabs_fuooku = new Audio('https://icephy.github.io/project0522/audio/sound/izakaya/vocab_section/izakayaVocabSectionTabs_fuooku.mp3');
+        izakayaVocabSectionTabs_fuooku.play();
+      break;
+    case 'izakayaVocabSectionTabs_naifu':
+        var izakayaVocabSectionTabs_naifu = new Audio('https://icephy.github.io/project0522/audio/sound/izakaya/vocab_section/izakayaVocabSectionTabs_naifu.mp3');
+        izakayaVocabSectionTabs_naifu.play();
+      break;
+    case 'izakayaVocabSectionTabs_napukin':
+        var izakayaVocabSectionTabs_napukin = new Audio('https://icephy.github.io/project0522/audio/sound/izakaya/vocab_section/izakayaVocabSectionTabs_napukin.mp3');
+        izakayaVocabSectionTabs_napukin.play();
+      break;
+    case 'izakayaVocabSectionTabs_osara':
+        var izakayaVocabSectionTabs_osara = new Audio('https://icephy.github.io/project0522/audio/sound/izakaya/vocab_section/izakayaVocabSectionTabs_osara.mp3');
+        izakayaVocabSectionTabs_osara.play();
+      break;
+    case 'izakayaVocabSectionTabs_koppu':
+        var izakayaVocabSectionTabs_koppu = new Audio('https://icephy.github.io/project0522/audio/sound/izakaya/vocab_section/izakayaVocabSectionTabs_koppu.mp3');
+        izakayaVocabSectionTabs_koppu.play();
+      break;
+    case 'izakayaVocabSectionTabs_supuun':
+        var izakayaVocabSectionTabs_supuun = new Audio('https://icephy.github.io/project0522/audio/sound/izakaya/vocab_section/izakayaVocabSectionTabs_supuun.mp3');
+        izakayaVocabSectionTabs_supuun.play();
+      break;
+    case 'izakayaVocabSectionTabs_shushoku':
+        var izakayaVocabSectionTabs_shushoku = new Audio('https://icephy.github.io/project0522/audio/sound/izakaya/vocab_section/izakayaVocabSectionTabs_shushoku.mp3');
+        izakayaVocabSectionTabs_shushoku.play();
+      break;
+    case 'izakayaVocabSectionTabs_suupu':
+        var izakayaVocabSectionTabs_suupu = new Audio('https://icephy.github.io/project0522/audio/sound/izakaya/vocab_section/izakayaVocabSectionTabs_suupu.mp3');
+        izakayaVocabSectionTabs_suupu.play();
+      break;
+    case 'izakayaVocabSectionTabs_nomimono':
+        var izakayaVocabSectionTabs_nomimono = new Audio('https://icephy.github.io/project0522/audio/sound/izakaya/vocab_section/izakayaVocabSectionTabs_nomimono.mp3');
+        izakayaVocabSectionTabs_nomimono.play();
+      break;
+    case 'izakayaVocabSectionTabs_kampai':
+        var izakayaVocabSectionTabs_kampai = new Audio('https://icephy.github.io/project0522/audio/sound/izakaya/vocab_section/izakayaVocabSectionTabs_kampai.mp3');
+        izakayaVocabSectionTabs_kampai.play();
+      break;
+    case 'izakayaVocabSectionTabs_otoshi':
+        var izakayaVocabSectionTabs_otoshi = new Audio('https://icephy.github.io/project0522/audio/sound/izakaya/vocab_section/izakayaVocabSectionTabs_otoshi.mp3');
+        izakayaVocabSectionTabs_otoshi.play();
+      break;
+
+    // 單字區（二）
+
+
+    // 其他聲音的處理
+    default:
+      // 聲音未定義的情況
+      break;
+  }
+}
